656390901ad7159655bb2234d73bc5d0159ecb5b
narendra <106387808+narendra903@users.noreply.github.com>
2025-03-01 14:49:56 +0000

    agon version

--- app.py
+++ app.py
@@ -436,12 +436,12 @@ st.sidebar.markdown("""
 st.sidebar.markdown("---")
 st.sidebar.subheader("📞 Contact")
 st.sidebar.markdown("💡 Created by: **AI & Finance Enthusiasts**")
-st.sidebar.markdown("📩 Email: narendra.insights@gmail.com")
+#st.sidebar.markdown("📩 Email: narendra.insights@gmail.com")
 
 # Footer
 st.markdown("---")
 st.markdown(
-    "🛠️ **Built with AI & Love ❤️** | 📅 *Updated: 2025* | "
+    "🛠️ **Built with AI ** | 📅 *Updated: 2025* | "📩 *Email: narendra.insights@gmail.com*"
     "[<img src='https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg' width='20' height='20'>](https://www.linkedin.com/in/nk-analytics/)"
     " Connect on LinkedIn",
     unsafe_allow_html=True,
